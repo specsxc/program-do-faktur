7e58c51ebf8c4af0afa089fc042b3bff04616e29
specsxc <piotr.dolgoszyja1@gmail.com>
2024-01-29 16:10:21 +0100

    dodano płatności do pliku
    dodano płatności do pliku i poprawiono faktury_zapis

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import csv
 
 
 def faktury_zapis():
+    id_counter = 0
     while True:
         lista_walut = ['Euro', 'Dolar', 'Zloty']
         kwota = int(input("Wprowadź kwotę faktury: "))
@@ -15,13 +16,58 @@ def faktury_zapis():
         miesiac = int(input('Wprowadź miesiąc: '))
         dzien = int(input('Wprowadź dzień: '))
         data_wystawienia = date(rok, miesiac, dzien)
-        print("Kwota: ", kwota)
-        print("Twoja waluta to: ", waluta)
-        print("Data: ", data_wystawienia)
         # Zapisanie faktur do pliku
-        with open('faktury.csv', 'a', newline='') as faktury:
-            spamwriter = csv.writer(faktury, delimiter=',')
-            spamwriter.writerow({kwota, waluta, data_wystawienia})
+        id_counter += 1
+        naglowek = ['id', 'Kwota', 'Waluta', 'Data wystawienia']
+        with open('faktury.csv', mode='a', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=naglowek)
+            csv_file.seek(0, 2)  # Przesuń kursor na koniec pliku
+            rozmiar = csv_file.tell()
+            if rozmiar == 0:  # Dodaj nagłówki tylko jeśli plik jest pusty
+                writer.writeheader()
+            writer.writerow({'id': id_counter,
+                             'Kwota': kwota,
+                             'Waluta': waluta,
+                             'Data wystawienia': data_wystawienia})
+        print("Kwota faktury: ", kwota)
+        print("Waluta faktury: ", waluta)
+        print("Data: ", data_wystawienia)
 
 
+# Wprowadzanie danych płatnośći
+
+
+def platnosci_zapis():
+
+    id_counter = 0
+
+    while True:
+        lista_walut = ['Euro', 'Dolar', 'Zloty']
+        kwota = int(input("Wprowadź kwotę płatności: "))
+        print('Lista walut: ' + ' | '.join(lista_walut))
+        waluta = str(input("Wprowadź walute w jakiej została opłacona: "))
+        print("Kiedy została opłacona?")
+        rok = int(input('Wprowadź rok: '))
+        miesiac = int(input('Wprowadź miesiąc: '))
+        dzien = int(input('Wprowadź dzień: '))
+        data_platnosci = date(rok, miesiac, dzien)
+        # Zapisanie płatności do pliku
+        id_counter += 1
+        naglowek = ['id', 'Kwota', 'Waluta', 'Data płatności']
+        with open('platnosci.csv', mode='a', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=naglowek)
+            csv_file.seek(0, 2)  # Przesuń kursor na koniec pliku
+            rozmiar = csv_file.tell()
+            if rozmiar == 0:  # Dodaj nagłówki tylko jeśli plik jest pusty
+                writer.writeheader()
+            writer.writerow({'id': id_counter,
+                             'Kwota': kwota,
+                             'Waluta': waluta,
+                             'Data płatności': data_platnosci})
+        print("Kwota płatnośći: ", kwota)
+        print("Waluta płatnośći: ", waluta)
+        print("Data płatności: ", data_platnosci)
+
+
+platnosci_zapis()
 faktury_zapis()
